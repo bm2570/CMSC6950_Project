a826a5cb8648c33c43f1f84777cdea63ef252e8f
Brad McNiven <bm2570@mun.ca>
2021-06-11 13:28:08 -0230

    Updated heatplot to plot DM and redshift simultaneously

--- heatplot.py
+++ heatplot.py
@@ -2,35 +2,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
-# Load data from CSV
-dat = np.genfromtxt('latlon_hr_zhang.dat', delimiter=' ',skip_header=0)
-X_dat = dat[:,0]
-Y_dat = dat[:,1]
-Z_dat = dat[:,3]
+# Load in data
+dat = np.genfromtxt('latlon_inoue.dat', delimiter=' ',skip_header=0)
+lat = dat[:,0] #Latitude
+lon = dat[:,1] #Longitude
 
-
-# Convert from pandas dataframes to numpy arrays
+# Converting to np arrays
 X, Y, Z, = np.array([]), np.array([]), np.array([])
-for i in range(len(X_dat)):
-        X = np.append(X,X_dat[i])
-        Y = np.append(Y,Y_dat[i])
-        Z = np.append(Z,Z_dat[i])
+for i in range(len(lat)):
+        X = np.append(X,lat[i])
+        Y = np.append(Y,lon[i])
 
-# create x-y points to be used in heatmap
-xi = np.linspace(X.min(),X.max(),256)
-yi = np.linspace(Y.min(),Y.max(),256)
 
-# Z is a matrix of x-y values
-zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]), method='linear')
+# Creating x-y points to define the space (associated with z-coordinate)
+xi = np.linspace(X.min(),X.max(),300)
+yi = np.linspace(Y.min(),Y.max(),300)
 
-# I control the range of my colorbar by removing data 
-# outside of my range of interest
-#zmin = 0
-#zmax = 12
-#zi[(zi<zmin) | (zi>zmax)] = None
+for i in range(2):
 
-# Create the contour plot
-CS = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow)#,vmax=zmax, vmin=zmin)
-plt.colorbar()  
-plt.show()
+    if i==0: ## DM contribution
+        Z_dat = dat[:,2] #
+        Z = np.array([])
+        for i in range(len(lat)):
+            Z = np.append(Z,Z_dat[i])
+
+        # Need to make a 2d array for z axis
+        zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]))
+        
+        
+        # Creating heat map
+#        plt.figure(1)
+        fig1=plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow)#,vmax=zmax, vmin=zmin)
+        cbar=plt.colorbar()  
+        cbar.set_label('DM [pc cm$^{-3}$]')
+        plt.suptitle('Ioka2003')
+        plt.xlabel('Latitude')
+        plt.ylabel('Longitude')
+        #plt.annotate('(a)', xy=(2, 1), xytext=(-75, 70),fontsize=15)
+        plt.savefig('DM_heatplot_Ioka.png')
+        plt.show()
+    if i==1: ## Redshift
+        
+        Z_dat = dat[:,3] #
+        Z = np.array([])
+        for i in range(len(lat)):
+            Z = np.append(Z,Z_dat[i])
+        # Need to make a 2d array for z axis
+        zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]))
+            
+        # Creating heat map
+#        plt.figure(2)
+        fig2=plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow)#,vmax=zmax, vmin=zmin)
+        cbar=plt.colorbar()  
+        cbar.set_label('Redshift')
+        plt.suptitle('Ioka2003')
+        plt.xlabel('Latitude')
+        plt.ylabel('Longitude')
+        plt.savefig('redshift_heatplot_Ioka.png')
+        plt.show()
 
