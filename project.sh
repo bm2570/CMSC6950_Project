36f68a455833ce0d61fb0708e36fb3b099fa0d04
Brad McNiven <bm2570@mun.ca>
2021-06-13 13:23:58 -0230

    Added project shell script

--- project.sh
+++ project.sh
@@ -1,1 +1,15 @@
-#fill me
+python3 distance_calculations.py
+python3 distance_plot.py
+python3 latlong.py
+python3 heatplot.py latlon_inoue.dat Z
+python3 heatplot.py latlon_inoue.dat DM
+python3 heatplot.py latlon_zhang.dat Z
+python3 heatplot.py latlon_zhang.dat DM
+python3 heatplot.py latlon_ioka.dat Z
+python3 heatplot.py latlon_ioka.dat DM
+awk '$1==-9.090909090909065071e-01 {print}' latlon_inoue.dat > latcut_inoue.dat
+awk '$1==-9.090909090909065071e-01{print}' latlon_zhang.dat > latcut_zhang.dat
+awk '$1==-9.090909090909065071e-01{print}' latlon_ioka.dat > latcut_ioka.dat
+python3 plot_latlongcuts.py
+
+rm *.dat
