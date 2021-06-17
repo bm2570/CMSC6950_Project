71cfd2be1f7b91c0ae3b1601921043b76f189640
Brad <bm2570@mun.ca>
2021-06-16 21:36:36 -0230

    Added sys args for makefile usage

--- distance_calculations.py
+++ distance_calculations.py
@@ -1,24 +1,17 @@
 ### This script calculates the redshift, comoving distance, and luminosity distance the "Zhang2018", "Ioka2003", and "Inoue2004" methods, which each input different cosmological parameters into the integrand F(z)
 import fruitbat
 import numpy as np
-dat_zhang=[]
-dat_inoue=[]
-dat_ioka=[]
+import sys
 
+fname=sys.argv[1]
+dat=[]
 ### Using linspace to choose different dispersion measure (dm) values:
 for i in np.linspace(10, 5000, num=500):
 
 ### Initializing fruitbat package for each of method
-    frb_zhang = fruitbat.Frb(i,method="Zhang2018")
-    frb_ioka = fruitbat.Frb(i,method="Ioka2003")
-    frb_inoue = fruitbat.Frb(i,method="Inoue2004")
-
+    frb_dat = fruitbat.Frb(i,method=fname)
 ### Appending
-    dat_zhang.append((i, frb_zhang.calc_redshift(method="Zhang2018"), frb_zhang.calc_comoving_distance().value, frb_zhang.calc_luminosity_distance().value ))
-    dat_inoue.append((i, frb_inoue.calc_redshift(method="Inoue2004"), frb_inoue.calc_comoving_distance().value, frb_inoue.calc_luminosity_distance().value ))
-    dat_ioka.append((i, frb_ioka.calc_redshift(method="Ioka2003"), frb_ioka.calc_comoving_distance().value, frb_ioka.calc_luminosity_distance().value ))
+    dat.append((i, frb_dat.calc_redshift(method=fname), frb_dat.calc_comoving_distance().value, frb_dat.calc_luminosity_distance().value ))
 
 ### Saving data to .dat files for later analysis
-np.savetxt('distance_zhang.dat',dat_zhang)
-np.savetxt('distance_inoue.dat',dat_inoue)
-np.savetxt('distance_ioka.dat',dat_ioka)
+np.savetxt('distance_'+fname+'.dat',dat)
--- latlong.py
+++ latlong.py
@@ -1,24 +1,17 @@
 ### This script calculates the dispersion measure and redshift over the range of latitude and longitude coordinates from [-90,90] degrees for the "Zhang2018", "Ioka2003", and "Inoue2004" methods
 import fruitbat
 import numpy as np
+import sys
 
-
-zhangdat=[]
-inouedat=[]
-iokadat=[]
+fname=sys.argv[1]
+dat=[]
 
 for i in np.linspace(-90,90,100):
     for j in np.linspace(-90,90,100):
         gl_var=str(i)
         gb_var=str(j)
-        frb_zhang = fruitbat.Frb(600,method="Zhang2018",gl=gl_var,gb=gb_var)
-        frb_inoue = fruitbat.Frb(600,method="Inoue2004",gl=gl_var,gb=gb_var)
-        frb_ioka = fruitbat.Frb(600,method="Ioka2003",gl=gl_var,gb=gb_var)
-        zhangdat.append((i,j,frb_zhang.calc_dm_galaxy().value,frb_zhang.calc_redshift(method="Zhang2018")))
-        iokadat.append((i,j,frb_ioka.calc_dm_galaxy().value,frb_ioka.calc_redshift(method="Ioka2003")))
-        inouedat.append((i,j,frb_inoue.calc_dm_galaxy().value,frb_inoue.calc_redshift(method="Inoue2004")))
+        frb_dat = fruitbat.Frb(600,method=fname,gl=gl_var,gb=gb_var)
+        dat.append((i,j,frb_dat.calc_dm_galaxy().value,frb_dat.calc_redshift(method=fname)))
 
-np.savetxt('latlon_zhang.dat',zhangdat)
-np.savetxt('latlon_inoue.dat',inouedat)
-np.savetxt('latlon_ioka.dat',iokadat)
+np.savetxt('latlon_'+fname+'.dat',dat)
 
